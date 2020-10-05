685edeacf4a001a275c27101e3f74fcf67a49d8c
JaviPrieto <javiprieto16@gmail.com>
2020-10-05 18:36:25 +0200

    Actualizando fichero cita.py

---
+++ src/cita.py
@@ -0,0 +1,22 @@
+class Cita:
+
+    def __init__(self, fecha,hora,pelado,tiempo,precio):
+        self.fecha = fecha
+        self.hora = hora
+        self.pelado = pelado
+        self.tiempo = tiempo
+        self.precio = precio
+
+    def resumenCita(self):
+        """Imprime datos de la cita por pantalla."""
+        print("Fecha: " + self.fecha)
+        print("Hora: " + self.hora)
+        print("Tipo de pelado: " + self.pelado)
+        print("Tiempo empleado: " + self.tiempo + "min")
+        print("Precio: " + self.precio + "euros")
+
+       
+
+
+cita = Cita("16","12:00","corte pelo","15","10")
+cita.resumenCita()
