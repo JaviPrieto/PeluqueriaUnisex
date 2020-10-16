15f7fc79e5499ba5fb9b97aab89a8900dfea3a3c
JaviPrieto <javiprieto16@gmail.com>
2020-10-16 10:41:59 +0200

    Subiendo fichero de test close #10

--- tests/test_cita.py
+++ tests/test_cita.py
@@ -9,4 +9,22 @@ import pytest
 def cita():
     return Cita("UnID","fecha","hora","pelado","tiempo","precio")
 
-    
+
+def test_getID():
+    assert cita.getID() == ""
+
+def test_getFecha():
+    assert cita.getFecha() == ""
+
+def test_getHora():
+    assert cita.getHora() == ""
+
+def test_getPelado():
+    assert cita.getPelado() == ""
+
+def test_getTiempo():
+    assert cita.getTiempo() == ""
+
+def test_getPrecio():
+    assert cita.getPrecio() == ""
+
