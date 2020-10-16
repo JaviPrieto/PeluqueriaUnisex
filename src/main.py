335967c1646c572b1e76db85b8b60541d213eaa8
Javier Prieto Infante <34689547+JaviPrieto@users.noreply.github.com>
2020-10-16 11:32:58 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 from cita import Cita
 
 def main():
-    cita = Cita("c001","16","12:00","corte pelo","15","10") 
+    cita = Cita("c001","16/10","12:00","corte pelo","15","10") 
     cita.resumenCita()
 
 if __name__ == "__main__":
