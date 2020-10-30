baa4f96c74ff6dba59e26a7e1d14a55ced4c93a9
JaviPrieto <javiprieto16@gmail.com>
2020-10-30 13:13:55 +0100

    Añadiendo funciones tests close #19

--- tests/test_cita.py
+++ tests/test_cita.py
@@ -9,7 +9,15 @@ import pytest
 def cita(): 
     return Cita("UnID","fecha","hora","pelado","tiempo","precio")
 
- 
+def test_cogerCita():
+	assert cita.getFecha() == "" 
+	assert cita.getHora() == "" 
+	assert cita.getPelado() == "" 
+	
+def test_modificarCita():
+	assert cita.getFecha() == "" 
+	assert cita.getHora() == "" 	
+
 def test_getID(): 
     assert cita.getID() == ""
 
